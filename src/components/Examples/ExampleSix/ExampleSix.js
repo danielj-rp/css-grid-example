82b00c4d83d2e8baccf4960eaa47a92c612ad813
danielj-rp <danielj@reactpoland.com>
2020-06-26 10:26:51 +0200

    example 6

--- src/components/Examples/ExampleSix/ExampleSix.js
+++ src/components/Examples/ExampleSix/ExampleSix.js
@@ -11,7 +11,19 @@ const ExampleSix = () => {
         description={""}
       />
       <ExampleArea>
-        <div className="container-6"></div>
+        <div className="container-6">
+          <div className="item1">1</div>
+          <div className="item2">2</div>
+          <div className="item3">3</div>
+          {/* <div className="item4">3</div>
+          <div className="item4">3</div>
+          <div className="item4">3</div>
+          <div className="item4">3</div>
+          <div className="item4">3</div>
+          <div className="item4">3</div>
+          <div className="item4">3</div>
+          <div className="item4">3</div> */}
+        </div>
       </ExampleArea>
     </>
   );
