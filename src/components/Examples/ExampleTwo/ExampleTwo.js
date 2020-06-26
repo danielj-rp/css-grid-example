31761865c8d8802189807e90f6efb6813a57f076
danielj-rp <danielj@reactpoland.com>
2020-06-26 12:30:45 +0200

    feat: basic 2

--- src/components/Examples/ExampleTwo/ExampleTwo.js
+++ src/components/Examples/ExampleTwo/ExampleTwo.js
@@ -11,17 +11,17 @@ const ExampleTwo = () => {
         description={""}
       />
       <ExampleArea>
-        {/* <div className="container-2">
-          <div className="e1"></div>
-        </div> */}
         <div className="container-2">
+          <div className="e1"></div>
+        </div>
+        {/* <div className="container-2">
           <div className="e1">1</div>
           <div className="e2">2</div>
           <div className="e3">3</div>
           <div className="e4">4</div>
           <div className="e5">5</div>
           <div className="e6">6</div>
-        </div>
+        </div> */}
       </ExampleArea>
     </>
   );
